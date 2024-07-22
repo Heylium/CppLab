2e0e2cdfa2e00e45ccf8c29fb6ab1e51cb8ae107
Heylium <tuanhsiang@163.com>
2024-07-22 23:04:22 +0800

    Testing data generator added.

--- SelectionSort/main.cpp
+++ SelectionSort/main.cpp
@@ -1,6 +1,9 @@
 #include <future>
 #include <iostream>
 
+#include "SortTestHelper.h"
+#include "Student.h"
+
 
 template<typename T>
 void selectionSort(T arr[], int n) {
@@ -14,15 +17,22 @@ void selectionSort(T arr[], int n) {
     }
 }
 
-int main()
-{
+int main() {
     std::cout << "Hello, World!" << std::endl;
-    int a[10] = { 10, 9, 8,7,6,5,4,3,2,1};
-    selectionSort(a, 10);
-    for (int i = 0; i < 10; ++i) {
-        std::cout << a[i]<< " ";
-    }
+    // int a[10] = { 10, 9, 8,7,6,5,4,3,2,1};
+    int n = 10000;
+    int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+    selectionSort(arr, n);
+    SortTestHelper::printArray(arr, n);
     std::cout << std::endl;
+    delete[] arr;
+
+    Student d[4] = { {"D", 90}, {"C", 100}, {"B", 95}, {"A", 95} };
+    selectionSort(d, 4);
+    for (int i = 0; i < 4; i++) {
+        std::cout << d[i];
+    }
+    std::cout << endl;
 
     return 0;
 }
