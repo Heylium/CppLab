227e98f1a7892aea844450a37db9d1a2d831c633
Heylium <tuanhsiang@163.com>
2024-08-01 00:31:19 +0800

    MergeSort added.

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -6,26 +6,29 @@ using namespace std;
 
 template<typename T>
 void __merge(T arr[], int l, int mid, int r) {
-    T aux[r - l + 1];
+    // T aux[r - l + 1];
+    T *aux = new T[r-l+1];
     for (int i = l; i <= r; ++i) {
         aux[i-l] = arr[i];
     }
-    int i = l, j = mid + 1;
-    for (int k = l; k <= r; ++k) {
-        if (i > mid) {
-            arr[k] = aux[j-l];
-            j++;
-        } else if (j > r) {
-            arr[k] = aux[i-l];
-            i++;
-        } else if (aux[i-l] < aux[j-l]) {
-            arr[k] = aux[i-l];
-            i++;
-        } else {
-            arr[k] = aux[j-l];
-            j++;
+
+    int i = l, j = mid+1;
+    for( int k = l ; k <= r; k ++ ){
+
+        if( i > mid ){  // 如果左半部分元素已经全部处理完毕
+            arr[k] = aux[j-l]; j ++;
+        }
+        else if( j > r ){  // 如果右半部分元素已经全部处理完毕
+            arr[k] = aux[i-l]; i ++;
+        }
+        else if( aux[i-l] < aux[j-l] ) {  // 左半部分所指元素 < 右半部分所指元素
+            arr[k] = aux[i-l]; i ++;
+        }
+        else{  // 左半部分所指元素 >= 右半部分所指元素
+            arr[k] = aux[j-l]; j ++;
         }
     }
+    delete[] aux;
 }
 
 template<typename T>
@@ -42,7 +45,7 @@ void __mergeSort(T arr[], int l, int r) {
 template<typename T>
 void mergeSort(T arr[], int n) {
 
-
+    __mergeSort( arr , 0 , n-1 );
 }
 
 int main() {
@@ -55,6 +58,26 @@ int main() {
 
     SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
     SortTestHelper::testSort("Merge Sort",     mergeSort,     arr2, n);
+    delete[] arr1;
+    delete[] arr2;
+
+    cout<<endl;
+    // 测试2 测试近乎有序的数组
+    // 对于近乎有序的数组, 数组越有序, InsertionSort的时间性能越趋近于O(n)
+    // 所以可以尝试, 当swapTimes比较大时, MergeSort更快
+    // 但是当swapTimes小到一定程度, InsertionSort变得比MergeSort快
+    int swapTimes = 10;
+    assert( swapTimes >= 0 );
+
+    cout<<"Test for nearly ordered array, size = "<<n<<", swap time = "<<swapTimes<<endl;
+    arr1 = SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
+    arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
+    SortTestHelper::testSort("Merge Sort",     mergeSort,     arr2, n);
+
+    delete[] arr1;
+    delete[] arr2;
 
     return 0;
 }
