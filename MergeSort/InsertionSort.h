8785186e9bc8f82d77e0a91594a46d0ce52229cf
Heylium <tuanhsiang@163.com>
2024-08-02 00:40:04 +0800

    MergeSort updated.

--- MergeSort/InsertionSort.h
+++ MergeSort/InsertionSort.h
@@ -25,4 +25,20 @@ void insertionSort(T arr[], int n){
     return;
 }
 
+// 对arr[l...r]范围的数组进行插入排序
+template<typename T>
+void insertionSort(T arr[], int l, int r){
+
+    for( int i = l+1 ; i <= r ; i ++ ) {
+
+        T e = arr[i];
+        int j;
+        for (j = i; j > l && arr[j-1] > e; j--)
+            arr[j] = arr[j-1];
+        arr[j] = e;
+    }
+
+    return;
+}
+
 #endif //MERGE_SORT_INSERTIONSORT_H
--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -1,52 +1,80 @@
 #include <iostream>
 #include "SortTestHelper.h"
 #include "InsertionSort.h"
+#include "MergeSort.h"
 
 using namespace std;
 
+// template<typename T>
+// void __merge(T arr[], int l, int mid, int r) {
+//     // T aux[r - l + 1];
+//     T *aux = new T[r-l+1];
+//     for (int i = l; i <= r; ++i) {
+//         aux[i-l] = arr[i];
+//     }
+//
+//     int i = l, j = mid+1;
+//     for( int k = l ; k <= r; k ++ ){
+//
+//         if( i > mid ){  // 如果左半部分元素已经全部处理完毕
+//             arr[k] = aux[j-l]; j ++;
+//         }
+//         else if( j > r ){  // 如果右半部分元素已经全部处理完毕
+//             arr[k] = aux[i-l]; i ++;
+//         }
+//         else if( aux[i-l] < aux[j-l] ) {  // 左半部分所指元素 < 右半部分所指元素
+//             arr[k] = aux[i-l]; i ++;
+//         }
+//         else{  // 左半部分所指元素 >= 右半部分所指元素
+//             arr[k] = aux[j-l]; j ++;
+//         }
+//     }
+//     delete[] aux;
+// }
+//
+// template<typename T>
+// void __mergeSort(T arr[], int l, int r) {
+//     if (l >= r) {
+//         return;
+//     }
+//     int mid = (l + r) / 2;
+//     __mergeSort(arr, l, mid);
+//     __mergeSort(arr, mid, r);
+//     __merge(arr, l, mid, r);
+// }
+//
+// template<typename T>
+// void mergeSort(T arr[], int n) {
+//
+//     __mergeSort( arr , 0 , n-1 );
+// }
+
+// 使用优化的归并排序算法, 对arr[l...r]的范围进行排序
 template<typename T>
-void __merge(T arr[], int l, int mid, int r) {
-    // T aux[r - l + 1];
-    T *aux = new T[r-l+1];
-    for (int i = l; i <= r; ++i) {
-        aux[i-l] = arr[i];
-    }
+void __mergeSort2(T arr[], int l, int r){
 
-    int i = l, j = mid+1;
-    for( int k = l ; k <= r; k ++ ){
-
-        if( i > mid ){  // 如果左半部分元素已经全部处理完毕
-            arr[k] = aux[j-l]; j ++;
-        }
-        else if( j > r ){  // 如果右半部分元素已经全部处理完毕
-            arr[k] = aux[i-l]; i ++;
-        }
-        else if( aux[i-l] < aux[j-l] ) {  // 左半部分所指元素 < 右半部分所指元素
-            arr[k] = aux[i-l]; i ++;
-        }
-        else{  // 左半部分所指元素 >= 右半部分所指元素
-            arr[k] = aux[j-l]; j ++;
-        }
-    }
-    delete[] aux;
-}
-
-template<typename T>
-void __mergeSort(T arr[], int l, int r) {
-    if (l >= r) {
+    // 优化2: 对于小规模数组, 使用插入排序
+    if( r - l <= 15 ){
+        insertionSort(arr, l, r);
         return;
     }
-    int mid = (l + r) / 2;
-    __mergeSort(arr, l, mid);
-    __mergeSort(arr, mid, r);
-    __merge(arr, l, mid, r);
+
+    int mid = (l+r)/2;
+    __mergeSort2(arr, l, mid);
+    __mergeSort2(arr, mid+1, r);
+
+    // 优化1: 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+    // 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
+    if( arr[mid] > arr[mid+1] )
+        __merge(arr, l, mid, r);
 }
 
 template<typename T>
-void mergeSort(T arr[], int n) {
+void mergeSort2(T arr[], int n){
 
-    __mergeSort( arr , 0 , n-1 );
+    __mergeSort2( arr , 0 , n-1 );
 }
+
 
 int main() {
     int n = 50000;
@@ -55,11 +83,14 @@ int main() {
     cout<<"Test for random array, size = "<<n<<", random range [0, "<<n<<"]"<<endl;
     int* arr1 = SortTestHelper::generateRandomArray(n,0,n);
     int* arr2 = SortTestHelper::copyIntArray(arr1, n);
+    int* arr3 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
     SortTestHelper::testSort("Merge Sort",     mergeSort,     arr2, n);
+    SortTestHelper::testSort("Merge Sort 2",   mergeSort2,    arr3, n);
     delete[] arr1;
     delete[] arr2;
+    delete[] arr3;
 
     cout<<endl;
     // 测试2 测试近乎有序的数组
@@ -72,12 +103,15 @@ int main() {
     cout<<"Test for nearly ordered array, size = "<<n<<", swap time = "<<swapTimes<<endl;
     arr1 = SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
+    arr3 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
     SortTestHelper::testSort("Merge Sort",     mergeSort,     arr2, n);
+    SortTestHelper::testSort("Merge Sort 2",   mergeSort2,    arr3, n);
 
     delete[] arr1;
     delete[] arr2;
+    delete[] arr3;
 
     return 0;
 }
